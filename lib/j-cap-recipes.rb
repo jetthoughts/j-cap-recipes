8bd28d171f9ecf31d977bb61f317254213f22d9d
Michael Nikitochkin <miry.sof@gmail.com>
2013-11-01 00:25:18 +0200

    :panda_face: Fixed the laod files pattern.

--- lib/j-cap-recipes.rb
+++ lib/j-cap-recipes.rb
@@ -1,3 +1,2 @@
-puts "hello"
-
-Dir.glob('lib/capistrano/tasks/*.rb').each { |r| p r; require r }
+rake_files_pattern =  File.expand_path("./j-cap-recipes/tasks/*.rake", File.dirname(__FILE__))
+Dir.glob(rake_files_pattern).each { |r| import r }
